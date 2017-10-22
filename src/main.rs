05a337c2c8f9a0545af3178815d4836ca38cd76f
Naftuli Kay <me@naftuli.wtf>
2017-10-22 16:11:51 -0700

    Working solution.

--- src/main.rs
+++ src/main.rs
@@ -1,72 +1,23 @@
-use std::collections::LinkedList;
-
-struct Tower {
-    name: String,
-    rings: LinkedList<Ring>
-}
-
-impl Tower {
-    fn new(name: &str) -> Self {
-        Tower {
-            name: String::from(name),
-            rings: LinkedList::new()
-        }
-    }
-
-    fn pop(&mut self) -> Ring {
-        self.rings.pop_front().unwrap()
-    }
-
-    fn push(&mut self, ring: Ring) {
-        let next = match self.rings.iter().next() {
-            Some(r) => r.size,
-            None    => 0
-        };
-
-        if next > ring.size {
-            panic!("Cannot push {} onto tower {}, last value in tower is {}", ring.size,
-                self.name, next);
-        }
-
-        self.rings.push_front(ring);
-    }
-
-    fn size(&self) -> u8 {
-        self.rings.len() as u8
-    }
-}
-
-impl std::fmt::Debug for Tower {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        write!(f, "Tower {}: {:?}", self.name, self.rings)
-    }
-}
-
-struct Ring {
-    size: u8
-}
-
-impl std::fmt::Debug for Ring {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        write!(f, "Ring({})", self.size)
-    }
-}
-
-impl Ring {
-    fn new(size: u8) -> Self {
-        Ring { size }
-    }
-}
-
-fn hanoi() {
-    let (mut a, mut b, mut c) = (Tower::new("A"), Tower::new("B"), Tower::new("C"));
-
-    // load the rings
-    for s in 1..4 {
-        a.rings.push_back(Ring::new(s));
+fn move_tower(i: u8, size: u8, source: &str, aux: &str, dest: &str) {
+    if size == 1 {
+        println!("[{}] move disk {} from {} to {}", i, size, source, dest);
+    } else {
+        move_tower(i + 1, size - 1, source, dest, aux);
+        println!("[{}] move disk {} from {} to {}", i, size, source, dest);
+        move_tower(i + 1, size - 1, aux, source, dest);
     }
 }
 
 fn main() {
-    hanoi();
+    println!("--- solution for size: 1 ---");
+    move_tower(0, 1, "Source", "Aux", "Dest");
+    println!();
+
+    println!("--- solution for size: 2 ---");
+    move_tower(0, 2, "Source", "Aux", "Dest");
+    println!();
+
+    println!("--- solution for size: 3 ---");
+    move_tower(0, 3, "Source", "Aux", "Dest");
+    println!();
 }
